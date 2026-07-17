a9058e3ba2e3b0db29ee8dc241fea36623ca10be
PJB3005 <pieterjan.briers+git@gmail.com>
2026-07-17 15:00:42 +0200

    Initial commit (only jampacked for now)

---
+++ README.md
@@ -0,0 +1,5 @@
+# chutools
+
+This is a collection of tools for working with audio assets in Nintendo games that use JAudio. It is effectively an updated version of [JAMTools](https://www.xayr.gay/tools/SoundModdingToolkit/) by XAYRGA.
+
+Development will primarily focus on features to aid [Dusklight](https://twilitrealm.dev/) modding, though we will do our best to accept contributions relevant to other games as well.
---
+++ jampacked/HelpManifest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jampacked
+{
+    public static class HelpManifest
+    {
+        public static void print_general()
+        {
+            Console.WriteLine("https://www.xayr.ga/");
+            Console.WriteLine("jampacked BAA/AAF packer");
+            Console.WriteLine("jampacked unpack <AAF/BAA/BA File> <outputFolder>");
+            Console.WriteLine("jampacked pack <jampacked Project Folder> <AAF/BAA/BX file>");
+        }
+    }
+
+}
